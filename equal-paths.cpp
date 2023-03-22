a7637a82244de03d7d0cf00b3d7de546c01ec162
Jiale Wang <jialewan@usc.edu>
2023-03-22 04:20:25 +0000

    equal-path

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -13,6 +13,27 @@ using namespace std;
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if(root==NULL) return true;
+    int left_height = findHeight(root->left);
+	int right_height = findHeight(root->right);
+    if(root->left!=NULL&&root->right==NULL)
+    return equalPaths(root->left)&&equalPaths(root->right);
+    else if(root->left==NULL&&root->right!=NULL)
+    return equalPaths(root->left)&&equalPaths(root->right);
+    else
+    if(left_height!=right_height) return false;
+    return equalPaths(root->left)&&equalPaths(root->right);
 
+}
+int findHeight(Node *root)
+{
+	if(root==NULL) 
+	return 0;
+	int left_height = findHeight(root->left);
+	int right_height = findHeight(root->right);
+	if(left_height>right_height) 
+	return left_height+1;
+	else 
+	return right_height+1;
 }
 
